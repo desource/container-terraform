d2911fa76530cd6c8df3b47fd84b226c38998cc4
Markus Kobler <markus.kobler@digital.hmrc.gov.uk>
2016-08-30 13:50:18 +0100

    Initial port over

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+set -euo pipefail
+
+version=${version:-hmrc-dev}
+out=${PWD}/out
+
+export GOPATH=${PWD}/go
+export PATH=${GOPATH}/bin:${PATH}
+
+build() {
+  mkdir -p ${out}/bin ${out}/tmp
+  
+  cd ${GOPATH}/src/github.com/hashicorp/terraform
+  
+  echo "Build Terraform"
+  make bin XC_OS="linux darwin" XC_ARCH="amd64"
+  
+  cp -r pkg ${out}/pkg
+}
+
+dockerfile() {
+  echo "Terraform version ${version}"
+  cat <<EOF > ${out}/tag
+${version}
+EOF
+  
+  echo "Terraform Dockerfile"
+  cat <<EOF | tee > ${out}/Dockerfile
+FROM alpine:3.4
+
+RUN apk --no-cache add git
+
+ADD pkg/linux_amd64/terraform /bin/terraform
+
+VOLUME ["/terraform"]
+
+ENTRYPOINT ["/bin/terraform"]
+
+EOF
+}
+
+build
+dockerfile
